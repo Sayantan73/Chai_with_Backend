b2add3e4006400420a5da56540a0b9cfbe16cbb0
Karan <sayantankaran73@gmail.com>
2025-04-24 21:16:14 +0530

    add some new models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,6 +22,7 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("change-password").post(verifyJwt, changeCurrentPassword)
 router.route("current-user").get(verifyJwt, getCurrentUser)
 router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+
 router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
 router.route("/channel/:username").get(verifyJwt, getUserChannelProfile)
